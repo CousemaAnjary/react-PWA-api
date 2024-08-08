c731c68d076363569bed731f1e34708dbc60e8bd
CousemaAnjary <anjaryabdillah@gmail.com>
2024-08-08 09:58:12 +0300

    chore: Update npm dependencies for React and React Router

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,20 @@
+export default function Home() {
+    /**
+     * ! STATE (état, données) de l'application
+     */
+
+
+    /**
+     * ! COMPORTEMENT (méthodes, fonctions) de l'application
+     */
+
+
+    /**
+     * ! AFFICHAGE (render) de l'application
+     */
+    return (
+        <>
+            <h1>Home</h1>
+        </>
+    )
+}
