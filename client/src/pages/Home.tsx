e315b8352d65e44c8b833aca364e697e5a25db95
CousemaAnjary <anjaryabdillah@gmail.com>
2024-08-08 10:10:01 +0300

    chore: Update npm dependencies for Tailwind CSS and PostCSS

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -14,7 +14,9 @@ export default function Home() {
      */
     return (
         <>
-            <h1>Home</h1>
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
         </>
     )
 }
