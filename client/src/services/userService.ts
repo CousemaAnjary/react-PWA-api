82363015c0a5c0c447f998ad9ce656428c57265d
CousemaAnjary <anjaryabdillah@gmail.com>
2024-08-08 19:03:30 +0300

    chore: Update npm dependencies for @hookform/resolvers, @radix-ui/react-label, react-hook-form, and zod

--- client/src/services/userService.ts
+++ client/src/services/userService.ts
@@ -1,11 +1,11 @@
 import api from "./config/apiConfig"
-
+import { UserType } from "@/typeScript/Type"
 
 // Inscrire un nouvel utilisateur
-export const register = async (dataRegister) => {
+export const createUser = async (dataUser: UserType) => {
     try {
         // Appel à l'API pour enregistrer un nouvel utilisateur
-        const response = await api.post('/register', dataRegister, { headers: { 'Content-Type': 'multipart/form-data' } })
+        const response = await api.post('/register', dataUser, { headers: { 'Content-Type': 'multipart/form-data' } })
         return response.data // Retourner les données de la réponse de l'API
 
     } catch (error) {
---
+++ client/src/typeScript/Type.ts
@@ -0,0 +1,9 @@
+
+// Le type des données d'user
+export type UserType = {
+    last_name: string
+    first_name: string
+    email: string
+    age: number
+    image?: File
+}
