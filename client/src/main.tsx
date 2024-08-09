263d80fb75b64a10e9c6de3c69b5b9920e9d8d1e
CousemaAnjary <anjaryabdillah@gmail.com>
2024-08-09 20:56:58 +0300

    chore: Update service worker to handle online/offline synchronization

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import { syncData } from "@/services/userService";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -11,3 +12,7 @@ createRoot(document.getElementById('root')!).render(
     </BrowserRouter>
   </StrictMode>,
 )
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('online', syncData);
+}
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -9,4 +9,16 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        serviceWorker: path.resolve(__dirname, 'src/service-worker.js'),
+      },
+      output: {
+        entryFileNames: '[name].js',
+      },
+    },
+  },
 })
