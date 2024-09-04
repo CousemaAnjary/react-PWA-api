79d83bd891ddcf85cf522fc305e1fd1acd0ab8e7
CousemaAnjary <anjaryabdillah@gmail.com>
2024-09-04 09:43:31 +0300

    chore: Update PWA configuration and manifest.json with additional icon sizes and types

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), VitePWA({
+    includeAssets: ['favicon.ico', 'apple-touch-icon-180x180', 'maskable-icon-512x512.png'],
     registerType: 'autoUpdate',
     injectRegister: false,
 
