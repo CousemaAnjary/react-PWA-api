de08b32be3f9a25d3d3503475dad0ada64e72c94
CousemaAnjary <anjaryabdillah@gmail.com>
2024-09-07 17:35:21 +0300

    chore: Update PWA configuration and manifest.json with additional icon sizes and types

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -11,30 +11,41 @@ export default defineConfig({
     registerType: 'autoUpdate',
     injectRegister: false,
 
+    pwaAssets: {
+      disabled: false,
+      config: true,
+    },
+
     manifest: {
       name: 'client',
       short_name: 'client',
       description: 'client',
       theme_color: '#ffffff',
-
-      icons: [{
-        src: 'pwa-64x64.png',
-        sizes: '64x64',
-        type: 'image/png',
-      }, {
-        src: 'pwa-192x192.png',
-        sizes: '192x192',
-        type: 'image/png',
-      }, {
-        src: 'pwa-512x512.png',
-        sizes: '512x512',
-        type: 'image/png',
-      }, {
-        src: 'maskable-icon-512x512.png',
-        sizes: '512x512',
-        type: 'image/png',
-        purpose: 'maskable',
-      }],
+      icons: [
+        {
+          src: "pwa-64x64.png",
+          sizes: "64x64",
+          type: "image/png"
+        },
+        {
+          src: "pwa-192x192.png",
+          sizes: "192x192",
+          type: "image/png"
+        },
+        {
+          src: "pwa-512x512.png",
+          sizes: "512x512",
+          type: "image/png"
+        },
+        {
+          src: "maskable-icon-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+          purpose: "maskable"
+        }
+      ],
+      start_url: '/',
+      display: 'standalone',
     },
 
     injectManifest: {
@@ -42,7 +53,7 @@ export default defineConfig({
     },
 
     devOptions: {
-      enabled: false,
+      enabled: true,
       navigateFallback: 'index.html',
       suppressWarnings: true,
       type: 'module',
