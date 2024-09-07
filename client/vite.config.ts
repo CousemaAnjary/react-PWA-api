9b84d9e8ca3fb5325a6cd50b3ffc005ce65636dc
CousemaAnjary <anjaryabdillah@gmail.com>
2024-09-07 17:11:36 +0300

    chore: Update PWA configuration and manifest.json with additional icon sizes and types

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -11,16 +11,30 @@ export default defineConfig({
     registerType: 'autoUpdate',
     injectRegister: false,
 
-    pwaAssets: {
-      disabled: false,
-      config: true,
-    },
-
     manifest: {
       name: 'client',
       short_name: 'client',
       description: 'client',
       theme_color: '#ffffff',
+
+      icons: [{
+        src: 'pwa-64x64.png',
+        sizes: '64x64',
+        type: 'image/png',
+      }, {
+        src: 'pwa-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      }, {
+        src: 'pwa-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      }, {
+        src: 'maskable-icon-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'maskable',
+      }],
     },
 
     injectManifest: {
@@ -28,7 +42,7 @@ export default defineConfig({
     },
 
     devOptions: {
-      enabled: true,
+      enabled: false,
       navigateFallback: 'index.html',
       suppressWarnings: true,
       type: 'module',
