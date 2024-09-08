ce3ee7c9e4e1eefd6566773f921e646e9975a0b2
CousemaAnjary <anjaryabdillah@gmail.com>
2024-09-08 19:55:05 +0300

    Refactoriser la configuration PWA et le fichier manifest.json pour supprimer les fichiers et configurations inutilisés

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -14,7 +14,9 @@ export default function App() {
    */
   return (
     <>
-      <h1>App</h1>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </>
   )
 }
