bea535451dd67bff2230be647def08be2baba214
CousemaAnjary <anjaryabdillah@gmail.com>
2024-09-12 21:25:35 +0300

    Refactoriser les composants Navbar et Dashboard pour ajouter la barre de navigation dans le tableau de bord

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,3 +1,6 @@
+import { Link } from "react-router-dom";
+import { Button } from "./ui/button";
+
 export default function Navbar() {
     /**
      * ! STATE (état, données) de l'application
@@ -14,19 +17,20 @@ export default function Navbar() {
      */
     return (
         <>
-            <nav>
-                <div className="logo">
-                    <img src="" alt="" />
-                </div>
+            <nav className="container-fluid p-4 bg-white border-b">
+                <div className="container flex justify-between items-center">
+                    <div className="logo ml-10">
+                        <h1 className="font-mono text-xl font-medium">Logo</h1>
+                    </div>
 
-                <div className="navbar">
-                    <ul>
-                        <li><a href="">Dashobard</a></li>
-                        <li><a href="">Project</a></li>
-                        <li><a href="">Contact</a></li>
-                    </ul>
+                    <div className="navbar">
+                        <ul className="flex gap-2 mr-10">
+                            <li><Link to="#"><Button variant={"ghost"} className="font-semibold">Dashobard</Button></Link></li>
+                            <li><Link to="#"><Button variant={"ghost"} className="font-semibold">To-do</Button></Link></li>
+                            <li><Link to="#"><Button variant={"ghost"} className="font-semibold">Users</Button></Link></li>
+                        </ul>
+                    </div>
                 </div>
-
             </nav>
         </>
     )
